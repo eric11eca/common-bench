520cc7d48d00a9626d799920fe044e4497e2dfc5
Zeming Chen <chenz16@rose-hulman.edu>
2022-12-14 11:26:00 +0100

    modify strategy

--- run.sh
+++ run.sh
@@ -6,17 +6,15 @@ MODEL_TYPE="t5"
 MODEL_NAME_OR_PATH="macaw-11b"
 TRAIN_BATCH_SIZE=4
 PREDICT_BATCH_SIZE=1
-N_GPU=1
+N_GPU=4
 
-python cli_maml.py \
+python main.py \
     --do_eval \
     --dataset ${DATASET} \
     --model_type ${MODEL_TYPE} \
-    --input_format ${INPUT_FORMAT} \
     --model_name_or_path ${MODEL_NAME_OR_PATH} \
     --train_batch_size ${TRAIN_BATCH_SIZE} \
     --predict_batch_size ${PREDICT_BATCH_SIZE} \
     --learning_rate 3e-5 \
     --wandb_name ${MODEL_NAME_OR_PATH}-${DATASET}-eval \
-    --n_gpu ${N_GPU} \
-    --device_idx 0
+    --n_gpu ${N_GPU}
