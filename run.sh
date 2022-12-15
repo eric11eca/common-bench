055cba48ae4b20cc1670b5841abce31c1eb64d9a
eric11eca <chenz16@rose-hulman.edu>
2022-12-15 01:53:19 +0100

    add command to download data from wandb

--- run.sh
+++ run.sh
@@ -3,14 +3,18 @@
 DATASET="tomi"
 TASK="tomi"
 MODEL_TYPE="t5"
-MODEL_NAME_OR_PATH="macaw-large"
+MODEL_NAME_OR_PATH="macaw-11b"
 TRAIN_BATCH_SIZE=4
 PREDICT_BATCH_SIZE=1
-N_GPU=4
+N_GPU=1
+
+mkdir -p data/${DATASET}
+wandb artifact get epfl_nlp_phd/data-collection/${DATASET}:v0 --root data/${DATASET}
 
 python main.py \
     --do_eval \
     --dataset ${DATASET} \
+    --task ${TASK} \
     --model_type ${MODEL_TYPE} \
     --model_name_or_path ${MODEL_NAME_OR_PATH} \
     --train_batch_size ${TRAIN_BATCH_SIZE} \
